6a938890ffcfce40be884588da405068c5c01f0a
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2024-01-05 12:13:05 -0300

    Adicionando a data no processamento

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,13 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+
+
 
 public class Main {
     public static final String PATH = "src/resource/";
@@ -23,27 +26,32 @@ public class Main {
 
         linhas.stream().forEach( e -> {
             String[] campos = e.split(",");
-            var t = new Transacao(campos[0], campos[1]);
-            transacoes.add(t);
+            var t = new Transacao(campos[0], campos[1], campos[2]);
+            if( t.data.getMonth() == Month.JANUARY) {
+                transacoes.add(t);
+            }
         });
         BigDecimal total = BigDecimal.ZERO;
-        List<BigDecimal> valores = transacoes.stream().map(t -> t.valor)
-                .collect(Collectors.toList());
+        List<BigDecimal> valores = transacoes.stream()
+                .map(t -> t.valor)
+                .toList();
 
         for (BigDecimal v: valores) {
            total = total.add(v);
         }
-        System.out.println(total.setScale(2));
-
+        System.out.printf(" O saldo da mês de Janeiro: R$" + total.setScale(2));
 
         sc.close();
     }
 
     public static class Transacao{
+
+        public LocalDate data;
         public String nome;
         public BigDecimal valor;
 
-        public Transacao(String nome, String valor) {
+        public Transacao(String data, String nome, String valor) {
+            this.data = LocalDate.parse(data);
             this.nome = nome;
             this.valor = new BigDecimal(valor);
         }
