5f20d0b33e7df23fee5d1c18428a6faab531293e
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2024-01-11 12:54:04 -0300

    Escrevendo test simples

--- src/test/java/com/alkimin/service/TransacaoServiceTest.java
+++ src/test/java/com/alkimin/service/TransacaoServiceTest.java
@@ -2,11 +2,20 @@ package com.alkimin.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransacaoServiceTest {
 
     @Test
-    void gerarTransacoes() {
+    void deveGerarTransacaoComSucesso() {
+        List<String> linhas = List.of("2022-12-05,Salario,5000");
+
+        TransacaoService transacaoService = new TransacaoService();
+        var transacoes = transacaoService.gerarTransacoes(linhas);
+        var transacao = transacoes.get(0);
+
+        assertEquals("Salario", transacao.getNome());
     }
 }
