423dac882900aa0844da70cf39d012187bc250fe
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2024-01-11 12:47:06 -0300

    Adicionando JUnit 5 no projeto

---
+++ src/test/java/com/alkimin/service/TransacaoServiceTest.java
@@ -0,0 +1,12 @@
+package com.alkimin.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransacaoServiceTest {
+
+    @Test
+    void gerarTransacoes() {
+    }
+}
